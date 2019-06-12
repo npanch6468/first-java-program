a4606255c3a73822c3e7a9cb8e3ad77c0e307ffc
npanch6468 <nagapanchumarty@gmail.com>
2019-06-12 09:54:22 -0400

    first commit

---
+++ src/helloWorld.java
@@ -0,0 +1,8 @@
+public class helloWorld {
+    public static void main(String [] args){
+
+        System.out.println("Hello, World!");
+        System.out.println("Today is June 12, 2018. I am alive!");
+        System.out.println("My name is Naga Panchumarty");
+    }
+}
